b0e564c298b2a56c9f43e624ed9c2f3498c54241
asherk7 <masherk03@hotmail.com>
2022-07-18 21:54:24 -0400

    minor changes

--- blackjack.py
+++ blackjack.py
@@ -4,15 +4,20 @@ BlackJack card game
 from random import shuffle
 
 class Card():
-
+    '''
+    Created a class for each card
+    '''
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
 
     def __str__(self):
         return f'This card is the {self.rank} of {self.suit}'
-    
+
 class Deck():
+    '''
+    Created a class for the deck
+    '''
     suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
 
     ranks = ['Two', 'Three', 'Four', 'Five', 'Six',
@@ -24,7 +29,7 @@ class Deck():
 
     def __init__(self):
         self.deck = self.shuffle_deck(self.create_deck())
-    
+
     def create_deck(self):
         all_cards = []
         for suit in Deck.suits:
@@ -38,12 +43,15 @@ class Deck():
 
     def __len__(self):
         return len(self.deck)
-    
+
     def __str__(self):
-        l = [f'{x} of {y}' for y in Deck.suits for x in Deck.ranks] #y is outer loop
-        return f'Cards in deck: {l}'
+        printable_deck = [f'{x} of {y}' for y in Deck.suits for x in Deck.ranks] #y is outer loop
+        return f'Cards in deck: {printable_deck}'
 
 class Player():
+    '''
+    Created a class for the players
+    '''
     def __init__(self, name, type, cards, money):
         self.name = name
         self.type = type #user or dealer
@@ -57,15 +65,18 @@ class Player():
     def __len__(self):
         return len(self.cards)
 
+#player can stand or hit
+#pick betting amount
+#player wins 3:2 amount of money
+#keep track of player record, tell them if they bust or get 21
+#add multiple players
+#double down doubles bet, and player only receives one card
+#can only double down after dealt first 2 cards
+
 def game():
+    #all that's left is game logic
     money_bet = 0
-    #player can stand or hit
-    #pick betting amount
-    #player wins 3:2 amount of money
-    #keep track of player record, tell them if they bust or get 21
-    #add multiple players
-    #double down doubles bet, and player only receives one card
-    pass
+    player_list = []
 
 if __name__ == '__main__':
     game()
