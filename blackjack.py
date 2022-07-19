af8d190ade82cd86b2a77a89fa85cc54e2c185ce
asherk7 <masherk03@hotmail.com>
2022-07-18 21:42:30 -0400

    new changes

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,6 @@
 '''
 BlackJack card game
 '''
-
 from random import shuffle
 
 class Card():
@@ -16,10 +15,10 @@ class Card():
 class Deck():
     suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
 
-    ranks = ['One', 'Two', 'Three', 'Four', 'Five', 'Six',
+    ranks = ['Two', 'Three', 'Four', 'Five', 'Six',
     'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
 
-    values = {'One':1, 'Two':2, 'Three':3, 'Four':4, 'Five':5,
+    values = {'Two':2, 'Three':3, 'Four':4, 'Five':5,
     'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
     'Queen':10, 'King':10, 'Ace':11}
 
@@ -34,25 +33,38 @@ class Deck():
         return all_cards
 
     def shuffle_deck(self, deck):
-        return shuffle(deck)
+        shuffle(deck)
+        return deck
 
     def __len__(self):
         return len(self.deck)
     
     def __str__(self):
-        return f'Cards in deck: {self.deck}'
+        l = [f'{x} of {y}' for y in Deck.suits for x in Deck.ranks] #y is outer loop
+        return f'Cards in deck: {l}'
 
 class Player():
-    #create 2 instances, user and dealer
-    def __init__(self, name, type):
+    def __init__(self, name, type, cards, money):
         self.name = name
         self.type = type #user or dealer
+        self.cards = cards
+        self.money = money
+        self.record = [0, 0, 0] #wins, losses, busts
+
+    def hit(self, card):
+        self.cards.append(card)
 
     def __len__(self):
-        #return len(player_cards)
-        pass
+        return len(self.cards)
 
 def game():
+    money_bet = 0
+    #player can stand or hit
+    #pick betting amount
+    #player wins 3:2 amount of money
+    #keep track of player record, tell them if they bust or get 21
+    #add multiple players
+    #double down doubles bet, and player only receives one card
     pass
 
 if __name__ == '__main__':
